266c5716ec6d44c2d6db762fab0bc3eb6504ee5f
r3a4 <racoronanunez@gmail.com>
2021-09-28 12:11:41 -0600

    I changed the cat's color to become a zombie

--- C11C-DrawPlay/src/Cat.java
+++ C11C-DrawPlay/src/Cat.java
@@ -27,23 +27,23 @@ public class Cat {
 		int x=catX;
 		int y=catY;
 		// Draw the head
-		g2.setColor(Color.pink);
+		g2.setColor(Color.green);
 		g2.fillOval(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
 		// Draw the eyes
-		g2.setColor(Color.blue);
+		g2.setColor(Color.yellow);
 		x = catX + EYE_X; 
 		y = catY + EYE_Y;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
 		x += EYE_SEPARATION;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
 		// Draw the mouth
-		g2.setColor(Color.orange);
+		g2.setColor(Color.red);
 		x = catX + MOUTH_X;
 		y = catY + MOUTH_Y;
 		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
 		g2.setColor(Color.black);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
-		g2.drawString("I am well", catX, catY+HEAD_DIMENSION+10);	
+		g2.drawString("I am a zombie", catX, catY+HEAD_DIMENSION+10);	
 	}
 }
