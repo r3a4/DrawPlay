7718d486b48a72c1d47773ee49a010d6a5cce10c
r3a4 <racoronanunez@gmail.com>
2021-09-28 12:07:07 -0600

    Changed text to I am sick and changed head dimension

--- C11C-DrawPlay/src/Cat.java
+++ C11C-DrawPlay/src/Cat.java
@@ -15,7 +15,7 @@ public class Cat {
 	private static final int EYE_WIDTH = 15;
 	// pick mouth height, width is based on head dimension
 	private static final int MOUTH_HEIGHT = 10;
-	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
+	private static final int MOUTH_WIDTH = HEAD_DIMENSION/3;
 	// mouth starts about 40% from left edge of head
 	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
 	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 3;
@@ -44,6 +44,6 @@ public class Cat {
 		g2.setColor(Color.black);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
-		g2.drawString("woof", catX, catY+HEAD_DIMENSION+10);	
+		g2.drawString("I am sick", catX, catY+HEAD_DIMENSION+10);	
 	}
 }
