d81863fb5c49454936d16e39052fb1e57ced6483
bgranmoeMines <bgranmoe@mines.edu>
2021-09-28 12:05:13 -0600

    Modified sound of cat to woof, and changed color of cat to green

--- C11C-DrawPlay/src/Cat.java
+++ C11C-DrawPlay/src/Cat.java
@@ -5,7 +5,7 @@ import java.awt.geom.*;
 public class Cat {
 	// drawing constants are private - noone needs to know what we're doing
 	// pick a head dimension
-	private static final int HEAD_DIMENSION = 100;
+	private static final int HEAD_DIMENSION = 150;
 	// eyes will be about 1/4 from top of head and 1/4 from left
 	private static final int EYE_Y = HEAD_DIMENSION/4;
 	private static final int EYE_X = HEAD_DIMENSION/4;
@@ -27,7 +27,7 @@ public class Cat {
 		int x=catX;
 		int y=catY;
 		// Draw the head
-		g2.setColor(Color.blue);
+		g2.setColor(Color.green);
 		g2.fillOval(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
 		// Draw the eyes
 		g2.setColor(Color.red);
@@ -44,6 +44,6 @@ public class Cat {
 		g2.setColor(Color.black);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
-		g2.drawString("Meow", catX, catY+HEAD_DIMENSION+10);	
+		g2.drawString("woof", catX, catY+HEAD_DIMENSION+10);	
 	}
 }
